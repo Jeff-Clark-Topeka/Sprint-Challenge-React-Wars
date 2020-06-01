aee8fb34ae90cc90b6daaea19f071d06b8bb5120
Jeff-Clark-Topeka <jeffclarktopeka@yahoo.com>
2020-05-31 22:50:54 -0500

    began building the character component

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,22 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import App from '../App'
+
+const Character = () => {
+
+    const [characters, setCharacters] = useState([])
+
+    useEffect(() => {
+        axios
+            .get('https://swapi.py4e.com/api/people/1/')
+            .then(response => {
+                console.log(response)
+                setCharacters(response.data)
+            })
+
+            .catch(error => console.log('error', error))
+    }, []);
+}
+
+export default Character;
